2744a797c9ab67a45f6c289676600e1a007530ef
kukku723 <z03177279@gmail.com>
2020-04-20 23:45:58 +0900

    down ruby version && add migration file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ruby:2.7.0
+FROM ruby:2.6.5
 
 RUN mkdir /app
 ADD ./Gemfile /app/Gemfile
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_18_080458) do
+ActiveRecord::Schema.define(version: 2020_04_18_172200) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -23,11 +23,12 @@ ActiveRecord::Schema.define(version: 2020_04_18_080458) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
