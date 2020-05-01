b51ccfecf6b219b01e64fbdfb3fc17dedfc208c8
kukku723 <z03177279@gmail.com>
2020-05-01 23:41:23 +0900

    update webpackのこと何もわかっていなかった。。。試行錯誤注

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -12,5 +12,15 @@ const dotenvFiles = [
 dotenvFiles.forEach((dotenvFile) => {
     dotenv.config({ path: dotenvFile, silent: true })
 });
+// sass-loaderの設定を取得
+const sassLoaders = environment.loaders.get('sass');
+const sassLoaderConfig = sassLoaders.use.filter(config => config.loader === 'sass-loader')[0];
+
+// sass-loaderに自動ロードの設定をがっちゃんこする
+Object.assign(sassLoaderConfig.options, {
+    data: "@import 'global-imports.scss';",
+    includePaths: [path.resolve(__dirname, '../../app/javascript/stylesheets/')]
+});
+
 
 module.exports = environment;
