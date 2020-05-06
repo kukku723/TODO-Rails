e8fc17730186fbc201194cc20785d5d6c2ba826d
kukku723 <z03177279@gmail.com>
2020-05-06 21:27:31 +0900

    update webpacker 草生やす

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,17 +1,6 @@
 const { environment } = require('@rails/webpacker');
-const webpack = require('webpack');
-const dotenv = require('dotenv');
+const path = require('path');
 
-const dotenvFiles = [
-    `.env.${process.env.NODE_ENV}.local`,
-    '.env.local',
-    `.env.${process.env.NODE_ENV}`,
-    '.env'
-];
-
-dotenvFiles.forEach((dotenvFile) => {
-    dotenv.config({ path: dotenvFile, silent: true })
-});
 // sass-loaderの設定を取得
 const sassLoaders = environment.loaders.get('sass');
 const sassLoaderConfig = sassLoaders.use.filter(config => config.loader === 'sass-loader')[0];
@@ -21,6 +10,4 @@ Object.assign(sassLoaderConfig.options, {
     data: "@import 'global-imports.scss';",
     includePaths: [path.resolve(__dirname, '../../app/javascript/stylesheets/')]
 });
-
-
 module.exports = environment;
