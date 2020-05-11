95a98d0e8037154c6c5de48f3845f82e5621582a
kukku723 <z03177279@gmail.com>
2020-05-11 22:56:04 +0900

    add vue

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
 const { environment } = require('@rails/webpacker');
+const { VueLoaderPlugin } = require('vue-loader')
+const vue = require('./loaders/vue')
 const path = require('path');
 
 // sass-loaderの設定を取得
@@ -10,4 +12,6 @@ Object.assign(sassLoaderConfig.options, {
     data: "@import 'global-imports.scss';",
     includePaths: [path.resolve(__dirname, '../../app/javascript/stylesheets/')]
 });
+environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
+environment.loaders.prepend('vue', vue)
 module.exports = environment;
